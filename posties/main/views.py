6c45e3009048c081cf473bb372114a3f5288b0bc
Dylan Auslander <xxoddgamerzxx@gmail.com>
2024-02-15 17:43:44 -0500

    Added base and home templates

--- posties/main/views.py
+++ posties/main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 # Create your views here.
+def home(response):
+    return render(response, "main/home.html")
+
+def v1(response):
+    return HttpResponse("<h1>view 1</h1>")
