a4b61187230bd08d81443902ae6a5cb5f76a7426
Dylan Auslander <xxoddgamerzxx@gmail.com>
2024-03-03 20:30:06 -0500

    Updated headers + posts to look more like sticky notes

--- main/models.py
+++ main/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Event(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255,null=True,blank=True)
+    name = models.CharField(max_length=300,null=True,blank=True)
     start = models.DateTimeField(null=True,blank=True)
     end = models.DateTimeField(null=True,blank=True)
     username = models.CharField(max_length=300)
