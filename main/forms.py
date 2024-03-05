9133ab4e948528ac892c7242825cc685d7ecc363
Dylan Auslander <xxoddgamerzxx@gmail.com>
2024-03-05 04:04:23 -0500

    Removed userID display on posts, updated account registration to alert users of when a username is taken + to redirect to login after a successful registration

---
+++ main/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ["username", "password1", "password2"]
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        try:
+            user = User.objects.exclude(pk=self.instance.pk).get(username=username)
+        except User.DoesNotExist:
+            return username
+        raise forms.ValidationError(u'Username "%s" is already in use.' % username)
