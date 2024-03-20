d564b4e26df598307909fe4ba97a54b2f7fe3c20
Dylan Auslander <xxoddgamerzxx@gmail.com>
2024-03-19 22:28:06 -0400

    Added editing your own posts

--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ from .forms import RegisterForm
 from django.http import JsonResponse
 from .models import Event
 from django.utils.timezone import datetime
+from dateutil.parser import parse
 
 # Create your views here.
 def home(response):
@@ -22,6 +23,7 @@ def all_events(request):
             'start': event.start.strftime("%m/%d/%Y, %H:%M:%S"),
             'end': event.end.strftime("%m/%d/%Y, %H:%M:%S"),
             'username': event.username,
+            'log_date': event.log_date.strftime("%#m/%#d/%Y, %I:%M:%S %p"),
         })
 
     return JsonResponse(out, safe=False)
@@ -32,13 +34,21 @@ def add_event(request):
     title = request.GET.get("title", None)
     username = request.GET.get("username", None)
     userID = request.user.id
-    log_date = datetime.now()
+    log_date = parse(request.GET.get("log_date", None))
     event = Event(name=str(title), start=start, end=end, username=str(username), userID=userID, log_date=log_date)
     event.save()
     data = {}
     return JsonResponse(data)
 
 def edit(request):
+    id = request.GET.get("id", None)
+    event = Event.objects.get(id=id)
+    title = request.GET.get("title", None)
+    username = request.GET.get("username", None)
+    if(username == event.username):
+        event.name = str(title)
+        event.save()
+    data = {}
     return JsonResponse(data)
 
 def remove(request):
