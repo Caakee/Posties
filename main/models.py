d9ae786439286373bd8df08ae41a8a475bcd2c4b
Dylan Auslander <xxoddgamerzxx@gmail.com>
2024-04-05 16:30:17 -0400

    Added a default bio value, fixed profiles so the correct profile pictures and bios now appear

--- main/models.py
+++ main/models.py
@@ -16,15 +16,12 @@ class Event(models.Model):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-
     avatar = models.ImageField(default='default.jpg', upload_to='profile_images')
-    bio = models.TextField()
+    bio = models.TextField(default="This user has no bio.")
 
     def save(self, *args, **kwargs):
         super().save()
-
         img = Image.open(self.avatar.path)
-
         if img.height > 200 or img.width > 200:
             new_img = (200, 200)
             img.thumbnail(new_img)
